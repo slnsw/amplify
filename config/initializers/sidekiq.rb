b57fd54c76f14ca11dfc924e59e08735a18bd2df
Geoffrey Roberts <geoffrey.roberts@sl.nsw.gov.au>
2018-09-18 10:54:41 +1000

    Added sidekiq/systemd info to readme

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,11 @@
 Sidekiq.configure_server do |config|
-  # config.redis = { url: ENV["REDIS_URL"] }
-  config.redis = { url: "redis://localhost:6379" }
+  config.redis = {
+    url: ENV["REDIS_URL"] || "redis://localhost:6379"
+  }
 end
 
 Sidekiq.configure_client do |config|
-  # config.redis = { url: ENV["REDIS_URL"] }
-  config.redis = { url: "redis://localhost:6379" }
+  config.redis = {
+    url: ENV["REDIS_URL"] || "redis://localhost:6379"
+  }
 end
