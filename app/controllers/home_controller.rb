e0e0d2d5d6f933555d6f5b695f888258f09b6559
sameera207 <sameera207@gmail.com>
2018-09-25 15:59:49 +1000

    WIP v2 home page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -10,11 +10,14 @@ class HomeController < ApplicationController
   def index
     @sort_list = SortList.list
     @themes = Theme.all
+    @collection = Collection.none
   end
 
   def transcripts
     @selected_collection_id = sort_params[:collection_id].to_i
     @selected_institution_id = select_institution_id
     @transcripts = TranscriptService.search(sort_params)
+    @themes = Theme.all
+    @sort_list = SortList.list
   end
 end
