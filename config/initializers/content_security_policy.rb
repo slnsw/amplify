cd77a1b34cca6d9b69675c743de6922b2ecec9f4
JonJon <neumerance@live.com>
2024-09-24 11:37:19 +0800

    fixing csp logger

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -1,11 +1,42 @@
 Rails.application.config.content_security_policy do |policy|
-  policy.default_src :self, :https
-  policy.font_src    :self, :https, :data
-  policy.img_src     :self, :https, :data
-  policy.object_src  :none
-  policy.script_src  :self, :https, 'http://www.googletagmanager.com', :unsafe_inline, :unsafe_eval
-  policy.style_src   :self, :https, :unsafe_inline
-  policy.connect_src :self, :https, 'http://localhost:3000', 'ws://localhost:3000'
+  policy.default_src  :self, :https
+  policy.font_src     :self, :https, :data
+  policy.img_src      :self,
+                      'https://*.google-analytics.com',
+                      'https://*.analytics.google.com',
+                      'https://*.googletagmanager.com',
+                      'https://*.g.doubleclick.net',
+                      'https://*.google.com',
+                      'https://*.google.au',
+                      :https,
+                      :data
+
+  policy.object_src   :none
+
+  policy.script_src   :self,
+                      :https,
+                      'https://*.googletagmanager.com',
+                      'https://*.google-analytics.com',
+                      'https://*.analytics.google.com',
+                      'https://*.googletagmanager.com',
+                      'https://*.g.doubleclick.net',
+                      'https://*.google.com',
+                      'https://*.google.au',
+                      :unsafe_inline,
+                      :unsafe_eval
+
+  policy.style_src    :self,
+                      :https,
+                      :unsafe_inline
+
+  policy.connect_src  :self,
+                      :https,
+                      'http://localhost:3000',
+                      'ws://localhost:3000'
+
+  policy.frame_src    :self,
+                      :https,
+                      'https://td.doubleclick.net'
 
   # Specify URI for violation reports
   policy.report_uri "/csp-violation-report-endpoint"
